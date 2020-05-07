f6664bcdf60c8ab2e1768557cc1c378460435dcc
m-joseph27 <muh.joseph27@gmail.com>
2020-05-07 20:11:34 +0700

    modified navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -108,7 +108,7 @@ export default {
       display: flex;
       justify-content: center;
       align-items: center;
-      cursor: pointer;
+      cursor: ;
       p{
         color: white;
         margin: 0;
