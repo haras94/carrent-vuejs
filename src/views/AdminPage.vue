928d5ce98f9802c890451c014b8e4d08114909e0
m-joseph27 <muh.joseph27@gmail.com>
2020-05-09 04:35:16 +0700

    add request card

--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -3,12 +3,6 @@
     <div class="adminPage">
       <div class="request">
         <h5>Request</h5>
-        <div class="requestPage" :class="{ 'dropdown': dropdown }">
-          <ul>
-            <li>Setting</li>
-            <li @click="logout" >Logout</li>
-          </ul>
-        </div>
       </div>
       <div class="rentaler">
         <h5>Rentaler</h5>
@@ -17,47 +11,22 @@
         <h5>All User</h5>
       </div>
     </div>
+    <CardRequest />
   </div>
 </template>
 
 <script>
+import CardRequest from '../components/base_/CardRequest.vue'
 
 export default {
   name: 'AdminPage',
-  data () {
-    return {
-      dropdown: false
-    }
+  components: {
+    CardRequest
   }
 }
 </script>
 
 <style lang="scss" scoped>
-a {
-  text-decoration: none;
-}
-.requestPage {
-  width: 170px;
-  height: 0px;
-  background-color: white;
-  position: absolute;
-  z-index: 21;
-  top: 100%;
-  right: 45px;
-  overflow: hidden;
-  transition: .2s;
-  border-radius: 3px;
-  li {
-    padding: 10px 20px;
-    cursor: pointer;
-    &:hover {
-      background-color: rgba(0, 0, 0, 0.061);
-    }
-  }
-  &.dropdown {
-    height: 100px;
-  }
-}
 .adminPageWrap{
   background-color: rgba(0, 0, 0, 0.055);
   width: 100%;
@@ -70,15 +39,16 @@ a {
     margin-top: 70px;
     background-color: rgba(255, 255, 255, 0.904);
     display: flex;
+    cursor: pointer;
     .request{
       width: 33.33%;
       height: 100%;
-      // background-color: blueviolet;
+      position: relative;
       display: flex;
       justify-content: center;
       align-items: center;
-      border-right: 1px solid rgba(0, 0, 0, 0.356);
-      border-bottom: 1px solid rgba(0, 0, 0, 0.356);
+      border-right: 1px solid rgba(0, 0, 0, 0.199);
+      border-bottom: 1px solid rgba(0, 0, 0, 0.199);
     }
     .rentaler{
       width: 33.33%;
@@ -87,8 +57,8 @@ a {
       display: flex;
       justify-content: center;
       align-items: center;
-      border-right: 1px solid rgba(0, 0, 0, 0.356);
-      border-bottom: 1px solid rgba(0, 0, 0, 0.356);
+      border-right: 1px solid rgba(0, 0, 0, 0.199);
+      border-bottom: 1px solid rgba(0, 0, 0, 0.199);
     }
     .allUser{
       width: 33.33%;
@@ -97,7 +67,7 @@ a {
       display: flex;
       justify-content: center;
       align-items: center;
-      border-bottom: 1px solid rgba(0, 0, 0, 0.356);
+      border-bottom: 1px solid rgba(0, 0, 0, 0.199);
     }
   }
 }
