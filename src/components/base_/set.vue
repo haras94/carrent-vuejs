130a0e64808ada9a588cf3bd47eb5584a08fcef4
akbarism <akbarismaiel@gmail.com>
2020-05-10 02:45:20 +0700

    fix edit image

--- src/components/base_/set.vue
+++ src/components/base_/set.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container-fluid">
     <aside class="side">
-      <img src="../../assets/img/isyana-sarasvati.jpg" alt="">
-      <pickFile/>
+      <img :src="this.image" alt="">
+      <input type="file" class="pick-files" ref="file" @change="editImage($event)">
     </aside>
     <div class="line">
       <h4>Biodata</h4>
@@ -12,14 +12,47 @@
 </template>
 
 <script>
-import pickFile from '../module_/pickFile.vue'
+// import { mapState } from 'vuex'
 import Form from '../base_/form.vue'
+import Axios from 'axios'
 
 export default {
   name: 'set',
   components: {
-    pickFile,
     Form
+  },
+  data () {
+    return {
+      image: null
+    }
+  },
+  // computed: {
+  //   ...mapState([
+  //     'userLogin'
+  //   ])
+  // },
+  methods: {
+    editImage (e) {
+      if (localStorage.id) {
+        if (localStorage.id.length !== 0) {
+          const file = this.$refs.file.files[0]
+          this.image = file
+          console.log(file)
+          const fd = new FormData()
+          fd.append('image', this.image)
+          Axios.patch(`${process.env.VUE_APP_API}user/upload/${localStorage.id}`, fd)
+            .then(res => {
+              const file = e.target.files[0]
+              const fr = new FileReader()
+              fr.onload = f => {
+                this.image = f.target.result
+              }
+              fr.readAsDataURL(file)
+              this.image.push(file)
+            })
+        }
+      }
+    }
   }
 }
 </script>
@@ -54,4 +87,36 @@ export default {
       padding: 10px;
     }
 }
+.pick-files {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    color: transparent;
+    width: 250px;
+    height: 47px;
+  }
+  .pick-files::-webkit-file-upload-button {
+    visibility: hidden;
+  }
+  .pick-files::before {
+    content: 'Pilih Gambar';
+    color: #ffffff;
+    display: inline-block;
+    width: 250px;
+    height: 50px;
+    background: #c82022;
+    outline: none;
+    white-space: nowrap;
+    -webkit-user-select: none;
+    cursor: pointer;
+    text-align: center;
+    padding: 10px;
+    font-weight: 600;
+  }
+  .pick-files:active {
+    outline: 0;
+  }
+  .pick-files:active::before {
+    background: -webkit-linear-gradient(top, #e3e3e3, #f9f9f9);
+  }
 </style>
